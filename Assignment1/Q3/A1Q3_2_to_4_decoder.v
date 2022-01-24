9c379f611e3409c6185c261fca13ce631f2ea199
Mshivam2409 <shivammalhotra08@gmail.com>
2022-01-24 22:59:38 +0530

    update comments

--- Assignment1/Q3/A1Q3_2_to_4_decoder.v
+++ Assignment1/Q3/A1Q3_2_to_4_decoder.v
@@ -2,9 +2,18 @@ module DECODER_2x4 (
     OUT, IN, EN
 );
     input [1 : 0] IN;
+    // Enable must be 1 for 2x4 Decoder to give correct values.
     input EN;
     output [3 : 0] OUT;
 
+    // The following assignment has been derived from K-Map of 2x4 decoder. Enabled has been added to ensure that decoder can be controlled externally.
+    //Enable	Inputs	   Outputs
+    // E	    I1	I0	O3	O2	O1	O0
+    // 0	    x	x	0	0	0	0
+    // 1	    0	0	0	0	0	1
+    // 1	    0	1	0	0	1	0
+    // 1	    1	0	0	1	0	0
+    // 1	    1	1	1	0	0	0
     assign OUT[0] = (~IN[0]) & (~IN[1]) & EN;
     assign OUT[1] = IN[0] & (~ IN[1]) & EN;
     assign OUT[2] = (~IN[0]) & IN[1] & EN; 
--- Assignment1/Q3/A1Q3_3_to_8_decoder.v
+++ Assignment1/Q3/A1Q3_3_to_8_decoder.v
@@ -7,7 +7,12 @@ module DECODER_3x8 (
     output [7 : 0] OUT;
     wire A2N;
 
+    // The parallel inputs IN1 & IN0 are applied to each 2 to 4 decoder. 
     assign A2N = ~IN[2];
+    // The input, IN2 is directly connected to Enable, EN of upper 2 to 4 decoder in order to get the outputs, OUT7 to OUT4. 
+    // These are the higher four min terms.
     DECODER_2x4 DECODER1 (OUT[7:4],IN[1:0],IN[2]);
+    // The complement of input IN2 is connected to Enable, EN of lower 2 to 4 decoder in order to get the outputs, OUT3 to OUT0. 
+    // These are the lower four min terms. 
     DECODER_2x4 DECODER2 (OUT[3:0],IN[1:0],A2N);
 endmodule
